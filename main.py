8baea959ee5c108bdf1f0f7435d7896f959982bc
KarimovProject <abdullatifkarimov772@gmail.com>
2022-08-16 08:56:24 +0500

    Main.py is ready

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 #Import library for telegram bot
 import requests
 
-TOKEN = '5355192064:AAHeVCobTRAUsm0lsi9e_l_QZbfFMVuVFeM'
+TOKEN = '5462386389:AAFy5Zz73zUqpEjcsNSJ3J1rmFh4sWl-Y08'
 
 #Send message 
 def send_message(text:str, chat_id:int):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    answer = requests.post(url, data={'chat_id': chat_id, 'text': text})
+    answer = requests.get(url, params = {'chat_id': chat_id, 'text': text})
     return answer.json()
 
 
 #Get updates
-def get_updates():
+def get_result()->list:
     url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
     answer = requests.get(url)
     data = answer.json()
@@ -20,7 +20,7 @@ def get_updates():
     
     return result
 
-def get_last_update(updates):
+def get_last_update(updates: list)->tuple:
     # Get last update
     update = updates[-1]
     # Get message text
@@ -33,20 +33,31 @@ def get_last_update(updates):
 
 
 # Last update id
-last_update_id = get_last_update(get_updates())[-1]
+last_update_id = get_last_update(get_result())[-1]
 #Send message through loop
 current_update_id = -1 #results[-1]['update_id']
+
+storage = ''
+
 while True:
     results = get_updates()
 
     if last_update_id != current_update_id:
-        print(last_update_id)
         text, chat_id ,current_update_id = get_last_update(results)
         send_message(text, chat_id)
         
         last_update_id = current_update_id
 
-    
+# while True:
+#     results = get_updates()
+
+#     if last_update_id != current_update_id:
+#         print(last_update_id)
+#         text, chat_id ,current_update_id = get_last_update(results)
+#         send_message(text, chat_id)
+        
+#         last_update_id = current_update_id
+
 
 # chat_id = '5575549228'
 # idx = 1
