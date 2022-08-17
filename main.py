e7287b8060e0c113bc80f577c7c60a66c57aeb56
KarimovProject <abdullatifkarimov772@gmail.com>
2022-08-17 18:34:01 +0500

    Last update

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import requests
 TOKEN = '5462386389:AAFy5Zz73zUqpEjcsNSJ3J1rmFh4sWl-Y08'
 
 #Send message 
-def send_message(text:str, chat_id:int):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    answer = requests.get(url, params = {'chat_id': chat_id, 'text': text})
+def send_photo(text:str, chat_id:int):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+    answer = requests.get(url, params = {'chat_id': chat_id, 'photo': photo})
     return answer.json()
 
+def send_message(text:str, chat_id:int):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    data={'chat_id': chat_id, 'text': text,'parse_mode': 'MarkdownV2'}
+    answer = requests.get(url,data)
+    return answer.json()
 
 #Get updates
 def get_result()->list:
@@ -25,11 +30,14 @@ def get_last_update(updates: list)->tuple:
     update = updates[-1]
     # Get message text
     text = update['message']['text']
+    # Get message photo
+    photo = update['message']['photo'][0]['file_id']
     # Get chat id
     chat_id = update['message']['chat']['id']
     # Get update id
     update_id = update['update_id']
-    return text, chat_id,update_id
+
+    return text, photo, chat_id,update_id
 
 
 # Last update id
@@ -37,14 +45,13 @@ last_update_id = get_last_update(get_result())[-1]
 #Send message through loop
 current_update_id = -1 #results[-1]['update_id']
 
-storage = ''
 
 while True:
-    results = get_updates()
+    results = get_result()
 
     if last_update_id != current_update_id:
-        text, chat_id ,current_update_id = get_last_update(results)
-        send_message(text, chat_id)
+        text, photo, chat_id ,current_update_id = get_last_update(results)
+        send_message(text, photo, chat_id)
         
         last_update_id = current_update_id
 
